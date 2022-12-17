b1d55b79c68d2644d4189c88d426195cd18b1e45
Aditya <chaurasiaaditya102@gmail.com>
2022-12-17 13:21:43 +0530

    first commit

---
+++ main.java
@@ -0,0 +1,37 @@
+// A computer is a dumb machine
+// It needs to be instrucred how to perform any particular task and we use a programming task and we use a programming language to achieve this
+
+// What is Java?
+// Java is a programming language which helps us write commands (code) to make the computer do a particular task
+// Java is a mediator between you and your computer
+
+// where is java used??
+// Java is used to instruct robots ti carry out a particular task
+// Used to create android apps
+// used to create desktop applications
+
+// how does java work?
+// 1) First, we write a list of commands
+// 2) Then the program is converted into an intermediate version, called bytecode. This version is not readable by humans anyone
+// 3) The bytecode is then read by a special software called a java Interpreter, which translates it to a machine friendly language
+// 4) The computer reads the translated code and perdorms the tasks requested.
+
+// our first Program
+
+// what is a computer program??
+// A computer program is a list of instructions that tells a computer what to do
+
+// The GOAT : Hello World!
+// the commmand used to print some text on screen is (System.out.print)
+// note: Java is a case sensitive language
+// System.out.ln("HelloWorld");
+
+/**
+ * main
+ * First Program
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.print("HelloWorld");
+    }
+}
